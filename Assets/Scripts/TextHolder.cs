203b86ff676b4498c3e804bdc1567ddfe4f41bfb
EQwon <bluetwintail0906@gmail.com>
2019-11-05 14:11:15 +0900

    기본 세팅

---
+++ Assets/Scripts/TextHolder.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextHolder : MonoBehaviour
+{
+    [SerializeField] private TextAsset text;
+
+    public List<List<string>> GetDialogue()
+    {
+        return Parser.DialogParse(text);
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private Text speakerName;
+    [SerializeField] private Text speech;
+
+    private TextHolder holder;
+    private List<List<string>> nowDialogue;
+    private int nowCnt = 0;
+
+    private void Awake()
+    {
+        holder = GetComponent<TextHolder>();
+    }
+
+    private void Start()
+    {
+        nowDialogue = holder.GetDialogue();
+        for (int i = 0; i < nowDialogue.Count; i++)
+        {
+            for (int j = 0; j < nowDialogue[i].Count; j++)
+            {
+                Debug.Log(i + ", " + j + " = " + nowDialogue[i][j]);
+            }
+        }
+        
+        ShowText();
+    }
+
+    private void ShowText()
+    {
+        speakerName.text = nowDialogue[nowCnt][0];
+        speech.text = nowDialogue[nowCnt][1];
+    }
+
+    public void NextText()
+    {
+        nowCnt += 1;
+        if (nowCnt >= nowDialogue.Count) return;
+
+        ShowText();
+    }
+}
