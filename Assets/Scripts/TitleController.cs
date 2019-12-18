aee97e4952a642fc4a1a71b24df803831e05a7a6
EQwon <bluetwintail0906@gmail.com>
2019-12-18 20:41:35 +0900

    타이틀 화면 추가

---
+++ Assets/Scripts/TitleController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TitleController : MonoBehaviour
+{
+    [SerializeField] private GameObject startButton;
+    [SerializeField] private GameObject creditButton;
+
+    public void MouseOver(RectTransform rect)
+    {
+        rect.localScale = new Vector2(1.5f, 1.5f);
+
+        if (rect.GetComponent<Animator>())
+        {
+            rect.GetComponent<Animator>().enabled = false;
+        }
+    }
+
+    public void MouseExit(RectTransform rect)
+    {
+        rect.localScale = new Vector2(1f, 1f);
+
+        if (rect.GetComponent<Animator>())
+        {
+            rect.GetComponent<Animator>().enabled = true;
+        }
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
